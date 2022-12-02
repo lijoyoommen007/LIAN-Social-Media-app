1dedbe35036cd0cfd0c246640bf4d9c8f1ab5b9e
Lijoy <oommenlijoy@gmail.com>
2022-12-03 01:09:15 +0530

    socket

--- ui/src/Pages/messenger/Messenger.jsx
+++ ui/src/Pages/messenger/Messenger.jsx
@@ -31,7 +31,7 @@ export default function Messenger() {
     const socket = useRef()
     
     useEffect(()=>{
-        socket.current = io('ws://localhost:8900');
+        socket.current = io('ws://socket.liansocialmedia.ml');
             socket.current.on("getMessage",(data)=>{
                 setArrivalMessage({
                     sender:data.senderId,
