4cc2352e0d0fad638e1bbe27230a45a8c09d66a6
Lijoy <oommenlijoy@gmail.com>
2022-12-02 20:04:11 +0530

    white list added

--- Backend/index.js
+++ Backend/index.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 app.use(express.json()); 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000","https://liansocialmedia.ml"],
   })
 ); 
 app.use(cookieParser());
