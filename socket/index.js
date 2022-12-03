e2191e8dee625e19ae2e4bf9afeeede627fde967
Lijoy <oommenlijoy@gmail.com>
2022-12-03 13:03:01 +0530

    socket err

--- socket/index.js
+++ socket/index.js
@@ -1,6 +1,6 @@
-const io = require("socket.io")(8900,('socket server running...'), {
+const io = require("socket.io")(8900,{
     cors: {
-      origin: ["http://localhost:3000","https://liansocialmedia.ml"]
+      origin: ["http://localhost:3000","https://liansocialmedia.ml"],
     },
   });
   
